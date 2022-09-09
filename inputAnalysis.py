730354ecb65fcc668d972edce426061dc80e1f30
Riley Carlin <rcarlin@usc.edu>
2022-09-08 22:28:25 -0700

    done with making sure no weirdly popular things

--- inputAnalysis.py
+++ inputAnalysis.py
@@ -26,6 +26,16 @@ def normalize(dicti, denom):
     return newDict
 
 
+# assuming train is first
+def printOutliers(dict1, dict2):
+    for k1 in dict1:
+        if k1 in dict2:
+            if abs(dict1[k1] - dict2[k1]) > .01:
+                print(k1, "train:", dict1[k1], "val:", dict2[k1])
+        #else:
+            # print(k1, "is not in val")
+
+
 def main():
 
     file = open('lang_to_sem_data.json')
@@ -45,7 +55,8 @@ def main():
     numVal = 0
 
 
-    # Getting the counts of
+    # Getting the counts of individual commands (actions or tokens) from both sets
+    # Training
     for bigSet in trainUntoken:
         for smallSet in bigSet:
             # temp = preprocess_string(smallSet[0])
@@ -62,6 +73,7 @@ def main():
                 trainTargetDict[smallSet[1][1]] = 1
             numTrain += 1
 
+    # Validation
     for bigSet in valUntoken:
         for smallSet in bigSet:
             # temp = preprocess_string(smallSet[0])
@@ -94,12 +106,61 @@ def main():
     pervalActDict = normalize(valActDict, numVal)
     pervalTargetDict = normalize(valTargetDict, numVal)
 
-    print(valActDict)
-    print("\n")
-    print(pervalActDict)
+    # print(valActDict)
+    # print("\n")
+    # print(pervalActDict)
+
+    # Most popular validation actions: GotoLocation, PickupObject, PutObject
+    # I am not surprised by those at all, those are pretty basic commands-- you can do them to many types objects
+    # (potentially more so than the other actions like heat or toggle) and these actions are common in strings of
+    # more complicated series of commands (if you are doing something with an object, you probably have to pick it up).
+    # All this to say I think these make sense for the most common --> worthwhile to practice more on more common things
+
+    # Least popular val actions: Toggle, clean, heat --> again, makes sense there's fewer of those because I can think
+    # of a lot of stuff that can't be toggled / shouldn't be heated up
+
+    # Most popular validation objects: countertop, diningtable, fridge, sinkbasin: Most common objects are places...
+    # guess that makes sense because actions often have specific locations you do them in? Also it was less obvious
+    # when I was just looking at the list of objects, but is this system mostly being tested on kitchen things? Is
+    # this a cooking robot? If the system performs better on kitchen related activites than not, I wouldn't be horribly
+    # surprised. That being said, these 4 objects are super low % so I don't think it's *that* skewed
+
+    # As expected, least common val object are non kitchen things like statue and watercan
+
+    # Most popular train actions: goto, pickup, put. Same as before. Logically makese sense as stated earlier. Glad
+    # it's lined up with val so far.
+
+    # Least popular train actions: toggle, clean, cool. Almost the same as val... not too worried because their
+    # percentages are almost identical.
+
+    # Most popular train locations: countertop, diningtable, sink, fridge. Out of order but still at the top--
+    # nothing to be worried about yet... No one thing is dominating the % which is good
+
+    # FIXME... maybe should have been looking also to see if skewed
+
+    # least popular train objects: glassbottle, plunger, winebottle... little concerning that some of the least common
+    # examples *are* kitchen stuff... I mean I know we shouldn't peak at the validation, but like if the intention is
+    # to use this system in a kitchen, shouldn't that inform what examples to give it?
+
+    # this is how I'm quickly filtering to find things to talk about
+    # for k in trainTargetDict:
+    #     if trainTargetDict[k] <700:
+    #         print(k, trainTargetDict[k], pertrainTargetDict[k])
 
 
+    # now to see if the % are way off
+    print("Actions:")
+    printOutliers(pertrainActDict, pervalActDict)
+    print("\n\nTargets")
+    printOutliers(pertrainTargetDict, pervalTargetDict)
 
+    # nothing was more than 10% off, except for things that weren't tested in validation... like the salt and pepper
+    # shakers *thinking emoji*
+
+    # Fridge, tomato, sidetable, apple, and bread are all off by >1% but <5%. I may be wrong, but that seems close
+    # enough for me
+
+    # and the action distributions are always close
 
     # make a heat map with size numactions * numtargets
 
